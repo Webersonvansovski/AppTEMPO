bf4f3f824515ef702975b002f80aa1ef1df6b3e5
Weberson Vansovski <116174741+Webersonvansovski@users.noreply.github.com>
2022-12-06 14:20:54 -0400

    Update main.py

--- main.py
+++ main.py
@@ -112,7 +112,7 @@ h = Label(text="...", font=("arial", 20, "bold"), bg='#1ab5ef')
 h.place(x=280, y=430)
 
 d = Label(text="...", font=("arial", 20, "bold"), bg='#1ab5ef')
-d.place(x=450, y=430)
+d.place(x=425, y=430)
 
 p = Label(text="...", font=("arial", 20, "bold"), bg='#1ab5ef')
 p.place(x=670, y=430)
